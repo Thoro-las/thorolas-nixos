51490146c288eaeee1acf217ff6d5d6c6749b755
thoroals <had.azouaou@gmail.com>
2025-08-16 18:20:25 +0100

    -

---
+++ scripts/hego.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  name = "hergo";
+  language = "python";
+
+  aliases = {
+    testo = "hergo";
+  };
+
+  script = ''
+  print("hego test in python")
+  '';
+}
---
+++ scripts/hergo.nix
@@ -0,0 +1,10 @@
+{ ... }:
+{
+  name = "hergo";
+  aliases = [ "herigo" "heroa" ];
+  language = "python";
+
+  script = ''
+  print("test in python")
+  '';
+}
